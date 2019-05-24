302f8006e32b8a1203d3120da376394ae9872a82
Luiz Araújo <luizhfreitas@outlook.com>
2019-05-23 21:16:12 -0300

    Adding sass suporte

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 //Boostrap Libs
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/css/bootstrap.css';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import './styles/scss/sb-admin-2.scss';
+
 //SB Admin 2 style
-import './assets/css/sb-admin-2.css';
+// import './assets/css/sb-admin-2.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
